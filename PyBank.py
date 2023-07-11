2b61643e8f79d7a0a6e5f1a3bb47b1e9e9c1307b
AA-Rafi <124394388+AA-Rafi@users.noreply.github.com>
2023-07-11 15:33:32 -0400

    Pybank Fixed
    Fixed change logic mistake to get correct values

--- PyBank.py
+++ PyBank.py
@@ -1,12 +1,11 @@
 import csv
-import os 
+import os
 
-budget_data_path = r"E:\\DataSci\\Class\\Unit 03 - Python\\Challenge\\Git\\Resources\\budget_data.csv"
+budget_data_path = os.path.join(r'E:\DataSci\Class\Unit 03 - Python\Challenge\Git\Resources\budget_data.csv')
 y = [] # this will house all the values of col 1 (Date), excluding the header
-x = [] #this will house all the values of col 2 (Profit/Loss), excluding the header
+x = [] # this will house all the values of col 2 (Profit/Loss), excluding the header
 
 with open(budget_data_path, 'r') as openfile:
-    
     csv_reader = csv.reader(openfile, delimiter=",")
     next(csv_reader)
 
@@ -14,47 +13,46 @@ with open(budget_data_path, 'r') as openfile:
         x.append(int(row[1]))
         y.append(row[0])
 
-    #Setting up "Total Months" and "Total" fields
+    # Setting up "Total Months" and "Total" fields
     sum_changes = sum(x) # Returns value for Total
-    total_months = sum_changes
-    months = len(x) #number of months
-    
-    #Setting up "Average Change" field
+    total_months = len(x) # number of months
+
+    # Setting up "Average Change" field
     change = [x[i] - x[i-1] for i in range(1, len(x))] 
     average_change = sum(change) / len(change)
 
-    #following code used to find max value AND associated month
-    max_value = max(change)
-    max_index = x.index(max_value)
-    max_month = y[max_index]
+    # Following code used to find max value AND associated month
+    max_change = max(change)
+    max_index = change.index(max_change)
+    max_month = y[max_index + 1]
 
-    #following code used to find min value AND associated month
-    min_value = min(change) 
-    min_index = x.index(min_value)
-    min_month = y[min_index]
+    # Following code used to find min value AND associated month
+    min_change = min(change) 
+    min_index = change.index(min_change)
+    min_month = y[min_index + 1]
 
-
-    #final statements that are printed for output
+    # Final statements that are printed for output
     print("Financial Analysis")
-    print("----------------------------")    
-    print(f"Total Months: {months}")  
-    print(f"Total: {total_months}")  
-    print(f"Average Change: {average_change} Although I know this isnt what we want")
-    print(f"Greatest Increase in Profits: {max_month} (${max_value})")  
-    print(f"Greatest Decrease in Profits: {min_month} (${min_value})")  
+    print("----------------------------")
+    print(f"Total Months: {total_months}")
+    print(f"Total: {sum_changes}")
+    print(f"Average Change: {average_change}")
+    print(f"Greatest Increase in Profits: {max_month} (${max_change})")
+    print(f"Greatest Decrease in Profits: {min_month} (${min_change})")
 
-    #now we move to put all these statements into a list for putting them into the txt file
+    # Now we move to put all these statements into a list for putting them into the txt file
     output = []
     output.append("Financial Analysis")
-    output.append("----------------------------")    
-    output.append(f"Total Months: {months}")  
-    output.append(f"Total: {sum_changes}")  
+    output.append("----------------------------")
+    output.append(f"Total Months: {total_months}")
+    output.append(f"Total: {sum_changes}")
     output.append(f"Average Change: {average_change}")
-    output.append(f"Greatest Increase in Profits: {max_month} (${max_value})")  
-    output.append(f"Greatest Decrease in Profits: {min_month} (${min_value})")  
+    output.append(f"Greatest Increase in Profits: {max_month} (${max_change})")
+    output.append(f"Greatest Decrease in Profits: {min_month} (${min_change})")
 
-    #we now make the file itself and write it with what values in the output to where ever your explorer is 
-    with open("pybank_output.txt", "w") as outfile:
+    # We now make the file itself and write it with what values in the output to wherever your explorer is
+    output_file_path = budget_data_path = os.path.join(r'E:\DataSci\Class\Unit 03 - Python\Challenge\Git\Analysis\pybank_output.txt')
+    with open(output_file_path, "w") as outfile:
         outfile.write('\n'.join(output))
 
-    
+    print(f"Output saved to {output_file_path}")
