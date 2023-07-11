efc9d588eddffabe13d23a9e7011e9f2fbc8b0ba
AA-Rafi <124394388+AA-Rafi@users.noreply.github.com>
2023-07-11 12:46:49 -0400

    Update PyBank.py

--- PyBank.py
+++ PyBank.py
@@ -1,10 +1,11 @@
 import csv
+import os 
 
-budget_data = r"PyBank\Resources\budget_data.csv"
-x = [] #this will house all the values of col 2 (Profit/Loss), excluding the header
+budget_data_path = r"E:\\DataSci\\Class\\Unit 03 - Python\\Challenge\\Git\\Resources\\budget_data.csv"
 y = [] # this will house all the values of col 1 (Date), excluding the header
+x = [] #this will house all the values of col 2 (Profit/Loss), excluding the header
 
-with open(budget_data, 'r') as openfile:
+with open(budget_data_path, 'r') as openfile:
     
     csv_reader = csv.reader(openfile, delimiter=",")
     next(csv_reader)
@@ -13,18 +14,22 @@ with open(budget_data, 'r') as openfile:
         x.append(int(row[1]))
         y.append(row[0])
 
-
-    sum_changes = sum(x) #incorrect numerator for Average
-    months = len(x) #denonator for Average
-    average_change = sum_changes/months #Average of Profit and Loss (placeholder)
+    #Setting up "Total Months" and "Total" fields
+    sum_changes = sum(x) # Returns value for Total
+    total_months = sum_changes
+    months = len(x) #number of months
+    
+    #Setting up "Average Change" field
+    change = [x[i] - x[i-1] for i in range(1, len(x))] 
+    average_change = sum(change) / len(change)
 
     #following code used to find max value AND associated month
-    max_value = max(x)
+    max_value = max(change)
     max_index = x.index(max_value)
     max_month = y[max_index]
 
     #following code used to find min value AND associated month
-    min_value = min(x)
+    min_value = min(change) 
     min_index = x.index(min_value)
     min_month = y[min_index]
 
@@ -33,7 +38,7 @@ with open(budget_data, 'r') as openfile:
     print("Financial Analysis")
     print("----------------------------")    
     print(f"Total Months: {months}")  
-    print(f"Total: {sum_changes}")  
+    print(f"Total: {total_months}")  
     print(f"Average Change: {average_change} Although I know this isnt what we want")
     print(f"Greatest Increase in Profits: {max_month} (${max_value})")  
     print(f"Greatest Decrease in Profits: {min_month} (${min_value})")  
@@ -44,7 +49,7 @@ with open(budget_data, 'r') as openfile:
     output.append("----------------------------")    
     output.append(f"Total Months: {months}")  
     output.append(f"Total: {sum_changes}")  
-    output.append(f"Average Change: {average_change} Although I know this isnt what we want")
+    output.append(f"Average Change: {average_change}")
     output.append(f"Greatest Increase in Profits: {max_month} (${max_value})")  
     output.append(f"Greatest Decrease in Profits: {min_month} (${min_value})")  
 
